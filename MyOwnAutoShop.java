14ea380a0a5e53779971d85b093011a44f7e88b2
Khasamoh <sharifakhelef@gmail.com>
2023-01-06 19:30:10 +0300

    Changing Instance Ford1 values

--- MyOwnAutoShop.java
+++ MyOwnAutoShop.java
@@ -1,7 +1,7 @@
 public class MyOwnAutoShop {
     public static void main(String[] args) {
         Sedan sedan = new Sedan(233,40000,"red",34);
-        Ford ford1 = new Ford(299,23000.0,"white",56,23000);
+        Ford ford1 = new Ford(45,23000.0,"white",20,23000);
         Ford ford2 = new Ford(100,4900.0,"Blac",35,50000);
         Car car = new Car(500,7900.5,"Yellow");
 
